9ba3735cfdd469e078f5e1b3def17f82de9edf45
James Harmison <jharmison@redhat.com>
2021-10-05 11:25:25 -0400

    Updated to add hostname in output

--- demo-application/hello-world/src/main.rs
+++ demo-application/hello-world/src/main.rs
@@ -1,15 +1,22 @@
 #[macro_use] extern crate rocket;
 
+fn hn() -> String {
+    hostname::get()
+        .unwrap()
+        .to_string_lossy()
+        .into_owned()
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!\n"
+fn index() -> String {
+
+    format!("Hello, world, from {}!\n", hn())
 }
 
 #[get("/hello/<name>")]
 fn hello(name: &str) -> String {
-    format!("Hello, {}!\n", name)
+    format!("Hello, {}, from {}!\n", name, hn())
 }
-
 
 #[launch]
 fn rocket() -> _ {
